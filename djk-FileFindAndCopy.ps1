1bdad0af78feec4d6684265f1eeb2965b5e1ecf6
Shaun <djkouza@yahoo.com>
2022-06-02 20:43:40 -0400

    Add files via upload

---
+++ djk-FileFindAndCopy.ps1
@@ -0,0 +1,31 @@
+﻿############################
+#   Script to 
+#   Author: DJKouza
+#   Contact:  https://twitter.com/djkouza
+#   Version: 1.0.0
+#   Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+############################
+
+$srcDir = Read-Host "Input directory root to search for files: "
+$destDir = $srcDir+"\output\"
+if (Test-Path -Path $destDir) 
+    {
+    #"Path exists!"
+    } 
+else 
+    {
+    New-Item -Path $destDir -ItemType Directory
+    }
+
+$inputList = Read-Host "Input location of the list of files to search for: "
+$fileList = Get-Content $inputList
+
+
+foreach($item in $fileList)
+    {
+    $nameToFind = [string]$item+".png"
+    $fileFound = Get-ChildItem $srcDir -Recurse | Where-Object { !($_.PSIsContainer) -and $_.Name.Equals($nameToFind)}
+    $fileDirecroty = $fileFound.DirectoryName
+    $fileFullPath = $fileDirecroty+"\"+$nameToFind
+    Copy-Item $fileFullPath -Container -Recurs -Destination $destDir
+    }
